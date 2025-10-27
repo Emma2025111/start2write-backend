37beac6a493db68318cde03a9cebd6ea77e5ca60
zohaib <mzohaibch.07@gmail.com>
2025-10-27 23:27:26 +0500

    first commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,6 +4,7 @@ import env from "./env.js";
 export async function connectDatabase() {
   mongoose.set("strictQuery", true);
   await mongoose.connect(env.mongoUri);
+  console.log("MongoDB connected successfully");
 }
 
 export default mongoose;
